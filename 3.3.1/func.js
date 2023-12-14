69b98ca51d3a366c62b4c635ec1ee2b6697a0d70
kimhyun <forhn37@gmail.com>
2023-12-15 04:51:54 +0900

    구조분해할당의 의미 재파악

--- 3.3.1/func.js
+++ 3.3.1/func.js
@@ -1,5 +1,8 @@
-const {odd, even} = require('./var')
+const yaho = require('./var')
+//let count = {odd, even}
+// const {odd, even} = {odd : '홀수', even : '짝수'}
 
+const {odd, even} =yaho;
 function checkOddOrEven(num) {
   if(num % 2) {
     return odd;
@@ -7,3 +10,9 @@ function checkOddOrEven(num) {
   return even;
 }
 module.exports = checkOddOrEven;
+
+
+// const {odd : odd, even : even } = { odd: 'cjs 홀수입니다. ', even: 'cjs 짝수입니다.' }
+
+// count.name = personName;
+// person.name = John
--- 3.3.1/var.js
+++ 3.3.1/var.js
@@ -1,7 +1,13 @@
 const odd = 'cjs 홀수입니다. ';
 const even = 'cjs 짝수입니다.';
 
-module.exports = {
+
+const yaho = {
   odd,
   even,
 }
+module.exports = yaho;
+// 변수를 프로퍼티로 넣어보낸다
+// 변수도 값을 가지고 있고, 프로퍼티도 값을 가지고 있으니 
+// 구조는 같다. 서로 치환하기 좋다
+console.log(yaho)
